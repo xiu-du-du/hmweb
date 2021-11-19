d9f1ceb9cf146fefeb6f6073c1b63c0d3ad3d326
xiu-du-du <cnimmor@gmail.com>
2021-11-20 02:28:25 +0800

    dom

--- all/html/DOM/封装dom库/src/dom.js
+++ all/html/DOM/封装dom库/src/dom.js
@@ -1,10 +1,11 @@
 /*
  * @Author: xiuxiu
  * @Date: 2021-11-18 22:30:50
- * @LastEditTime: 2021-11-19 11:50:17
+ * @LastEditTime: 2021-11-20 02:27:03
  * @FilePath: \all\html\DOM\封装dom库\src\dom.js
  */
 window.dom = {
+    /* 增 */
     // 创建元素
     create(string) {
         const container = document.createElement("template");
@@ -20,8 +21,8 @@ window.dom = {
         node.parentNode.insertBefore(node2, node)
     },
     // 添加一个儿子
-    append(parent, child) {
-        parent.appendChild(child)
+    append(parent, node) {
+        parent.appendChild(node)
     },
     // 添加一个父亲
     wrap(node, parent) {
@@ -34,17 +35,54 @@ window.dom = {
         node.parentNode.removeChild(node)
         return node
     },
+
+    /* 删 */
     //删除后代
     empty(node) {
-        // node.innerHTML=''  
-        // const childNodes = node.childNodes
-        // const { childNodes } = node //简写
+        // const { childNodes } = node
         const arr = []
         let x = node.firstChild
         while (x) {
-            arr.push(dom.remove(node.firstChild))
+            dom.remove(node.firstChild)
+            arr.push(node.firstChild)
             x = node.firstChild
         }
         return arr
+    },
+
+    /* 改 */
+    // 如果参数有三个，则为设置节点属性的属性值；如果参数有两个，则为读取节点属性的属性值
+    // 叫做重载
+    attr(node, name, value) {
+        if (arguments.length === 3) {
+            node.setAttribute(name, value)
+        } else if (arguments.length === 2) {
+            return node.getAttribute(name)
+        }
+    },
+    //修改元素内容
+    // 叫做适配
+    text(node, string) {
+        if (arguments.length === 2) {
+            if ('innerText' in node) {
+                node.innerText = string //ie
+            } else {
+                node.textContent = string // firefox/chrome
+            }
+        } else if (arguments.length === 1) {
+            if ('innerText' in node) {
+                return node.innerText //ie
+            } else {
+                return node.textContent // firefox/chrome
+            }
+        }
+    },
+    html(node, string) {
+        if (arguments.length === 2) {
+            console.log(string);
+            node.innerHTML = string
+        } else if (arguments.length === 1) {
+            return node.innerHTML
+        }
     }
 }
--- all/html/DOM/封装dom库/src/main.js
+++ all/html/DOM/封装dom库/src/main.js
@@ -1,14 +1,26 @@
 /*
  * @Author: xiuxiu
  * @Date: 2021-11-18 22:14:59
- * @LastEditTime: 2021-11-19 11:42:40
+ * @LastEditTime: 2021-11-20 02:27:22
  * @FilePath: \all\html\DOM\封装dom库\src\main.js
  */
+// 增
 // const div = dom.create('<div><span>222</span></div>')
 // const div = dom.create('<div>newDiv</div>')
 // dom.after(test, div)
 // const div3 = dom.create('<div id="parent">parent</div>')
 // dom.wrap(test, div3)
+// 删
 // console.log(dom.remove(test2));
-const nodes = dom.empty(window.empty)
-console.log(nodes);
+// console.log(window.empty);
+// console.log(dom.empty(window.empty));
+// dom.attr(window.empty, 'title', '123')
+// const title = dom.attr(window.empty, 'title')
+// console.log(`title: ${title}`);
+//改
+// console.log(dom.text(window.test));
+// dom.text(window.test, "123")
+// console.log(window.test.innerText);
+const test = window.test
+    // console.log(dom.html(test));
+    // dom.html(test, "231<span>123</span>312")
